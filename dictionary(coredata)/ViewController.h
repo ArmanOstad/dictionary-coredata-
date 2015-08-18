901551aaff17242d9cd51fad052cdf9b4674972a
arman <arman_empir@yahoo.com>
2015-08-18 19:57:36 +0430

    Initial Commit

---
+++ dictionary(coredata)/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  dictionary(coredata)
+//
+//  Created by Arman Ostad Mirzaei on 8/18/15.
+//  Copyright (c) 2015 Arman Ostad Mirzaei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ dictionary(coredata)Tests/dictionary_coredata_Tests.m
@@ -0,0 +1,40 @@
+//
+//  dictionary_coredata_Tests.m
+//  dictionary(coredata)Tests
+//
+//  Created by Arman Ostad Mirzaei on 8/18/15.
+//  Copyright (c) 2015 Arman Ostad Mirzaei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface dictionary_coredata_Tests : XCTestCase
+
+@end
+
+@implementation dictionary_coredata_Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
